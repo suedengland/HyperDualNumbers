e879cb3eae4e4c41846892c800bf8f6eeb14e8f4
Steven Becker <mail-stbe@protonmail.ch>
2021-03-16 13:48:55 +0100

    increment version number.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='HyperDualNumbers',
-    version='0.0.5',
+    version='0.1.0',
     author='Steven Becker',
     author_email='mail-stbe@protonmail.ch',
     packages=find_packages(),
